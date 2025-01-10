2760ce3e38010bf7e94740dc4c425902c428bf45
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-10 10:16:26 +0100

    New doxygen HTML made

--- doxygen/html/search/all_3.js
+++ doxygen/html/search/all_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['ledoff_0',['LEDoff',['../main_8cpp.html#a56485776eae912a719b64e5e6951335d',1,'main.cpp']]],
-  ['liquidcrystal_5fi2c_1',['LiquidCrystal_I2C',['../class_liquid_crystal___i2_c.html',1,'']]]
+  ['ledoff_0',['LEDoff',['../main_8cpp.html#a56485776eae912a719b64e5e6951335d',1,'main.cpp']]]
 ];
